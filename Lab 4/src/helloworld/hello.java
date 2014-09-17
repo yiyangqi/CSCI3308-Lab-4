bef1a0d8908007d8fa0266c9402f50b05c195163
yiyangqi <yiyang515@yahoo.com>
2014-09-17 15:47:53 -0600

    HOW 2 USE GIT PLSERINO

--- Lab 4/src/helloworld/hello.java
+++ Lab 4/src/helloworld/hello.java
@@ -4,5 +4,6 @@ public class hello {
 	public static void main(String[] args){
 		System.out.print("Hello World!");
 		System.out.print("How are you?");
+		System.out.print("One more line because we @#$%ed up.")
 	}
 }
