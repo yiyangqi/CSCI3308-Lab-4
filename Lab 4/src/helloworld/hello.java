00b4e401ebbbc5752bcf43196dee8eae1d86e8c5
yiyangqi <yiyang515@yahoo.com>
2014-09-17 15:52:41 -0600

    Change 3

--- Lab 4/src/helloworld/hello.java
+++ Lab 4/src/helloworld/hello.java
@@ -4,6 +4,6 @@ public class hello {
 	public static void main(String[] args){
 		System.out.print("Hello World!");
 		System.out.print("How are you?");
-		System.out.print("One more line because we @#$%ed up.")
+		System.out.print("One more line because we @#$%ed up.");
 	}
 }
